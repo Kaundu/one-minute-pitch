8c6744a4d8b74cf016073baacb639b8700f828c9
neville <kilonzoneville@gmail.com>
2018-07-03 12:45:50 +0300

    add the update profile form

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,4 +2,15 @@ class SignForm(FlaskForm):
 
     username = StringField('username',validators=[Required()])
     password = StringField('password', validators=[Required()])
-    submit = SubmitField("submit")
+    submit = SubmitField("submit")    submit = SubmitField("submit")
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class CategoryForm(FlaskForm):
+    """
+    class to create a form to create category
+    """
+    category_name = StringField('Pitch Category',validators=[Required()])
+    submit = SubmitField('Create')
