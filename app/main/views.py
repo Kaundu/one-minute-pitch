43c25f98b1030475335bdd0906fe1b55a2625d52
neville <kilonzoneville@gmail.com>
2018-07-03 12:17:41 +0300

    add the signin page views

--- app/main/views.py
+++ app/main/views.py
@@ -12,3 +12,13 @@ def index():
     title = 'Home'
 
     return render_template('index.html', title = title, categories = categories )
+   
+@main.route('/sign/', methods=['GET','POST'])
+def sign():
+    form = SignForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User(username = username , password = password)
+        return redirect(url_for('.sign'))
+    return render_template('sign.html', sign_form=form )
