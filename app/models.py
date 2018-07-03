f0295cbd2809f3babf6e60ffe01de35045d6b9f9
neville <kilonzoneville@gmail.com>
2018-07-03 12:14:13 +0300

    add the role class

--- app/models.py
+++ app/models.py
@@ -14,6 +14,16 @@ class User (UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.name}' class Category(db.Model):
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
 
     __tablename__ = 'categories'
 
