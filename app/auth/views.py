ab1b8f570e0a49aa156ddae0aa1f71d6a4ad80f7
neville <kilonzoneville@gmail.com>
2018-07-03 12:28:06 +0300

    add necessary imports for the auth/views file

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,11 @@
+from . import auth 
+from flask import render_template,redirect,url_for,request
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from .. import db
+from flask_login import login_user,logout_user,login_required
+
+
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
@@ -12,6 +20,8 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
@@ -33,3 +43,5 @@ def logout():
 
     title = "minute-pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+    
