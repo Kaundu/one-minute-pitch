b794bf9eedfb6b81ee9f7c4ded38bcc37c9ea2ed
neville <kilonzoneville@gmail.com>
2018-07-03 12:18:33 +0300

    add the sign up views

--- app/main/views.py
+++ app/main/views.py
@@ -21,4 +21,15 @@ def sign():
         password = form.password.data
         user = User(username = username , password = password)
         return redirect(url_for('.sign'))
-    return render_template('sign.html', sign_form=form )
+    return render_template('sign.html', sign_form=form )    return render_template('sign.html', sign_form=form )
+
+@main.route('/register', methods=["GET", "POST"])
+def register():
+   form = RegistrationForm()
+   if form.validate_on_submit():
+       user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+       db.session.add(user)
+       db.session.commit()
+       return redirect(url_for('auth.login'))
+       title = 'New Account'
+   return render_template('auth/register.html', registration_form=form)
